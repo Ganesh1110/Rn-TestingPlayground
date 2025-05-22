504bb1e4b2954602a72f2e8dc8392c52b666d596
Ganesh Jayaprakash <ganesh.jayaprakash@consultant.mannai.com.qa>
2025-05-22 11:59:19 +0530

    Add initial package.json for TestingPlayground project with dependencies and scripts

---
+++ App.js
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
